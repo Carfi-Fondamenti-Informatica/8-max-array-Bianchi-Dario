49196ef89fc1d6359e8d87e70b8218b6fbe13f83
Bianchi-Dario <91790218+Bianchi-Dario@users.noreply.github.com>
2021-12-11 19:22:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by bianc on 11/12/2021.
+//
+
+#ifndef UNTITLED13_LIB_H
+#define UNTITLED13_LIB_H
+float numeromassimo(const float numeri[], int dimensione);
+#endif //UNTITLED13_LIB_H
 
